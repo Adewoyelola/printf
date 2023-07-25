bec958972cfa89b09d828c6bf97813c377070bc8
Stanley Ibeneme <stanleyibeneme92@gmail.com>
2023-07-24 19:55:24 -0700

    m

--- convert.c
+++ convert.c
@@ -6,6 +6,7 @@
  * @base: input base
  * @lowercase: flag if hexa values need to be lowercase
  * Return: result string
+ * By Adewoyelola and Stanley
  */
 char *convert(unsigned long int num, int base, int lowercase)
 {
